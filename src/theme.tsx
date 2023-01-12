cd7fce20b94f1ba8bacc17a94f6aba02ad423913
Stefano Sambruna <stesambruna@gmail.com>
2023-01-13 00:12:41 +0100

    Updates

--- src/theme.tsx
+++
@@ -1,7 +0,0 @@
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Children } from "./design-system";
-
-export default function CustomTheme(props: { children: Children }) {
-  let theme = createTheme();
-  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
-}
